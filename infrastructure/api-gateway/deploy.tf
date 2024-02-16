b7c6c97a1fc9b9928e268434ac63207730b640bd
Brice Frisco <39070938+bricefrisco@users.noreply.github.com>
2024-02-16 14:38:42 -0600

    added /summoners resource

--- infrastructure/api-gateway/deploy.tf
+++ infrastructure/api-gateway/deploy.tf
@@ -63,3 +63,40 @@ resource "aws_lambda_permission" "api-summoner-permission" {
     principal = "apigateway.amazonaws.com"
     source_arn = "${aws_api_gateway_rest_api.default.execution_arn}/*/*"
 }
+
+/***************************
+ * Summoners API (/summoners)
+ ***************************/
+data "aws_lambda_function" "api-summoners" {
+  function_name = "api-summoners"
+}
+
+resource "aws_api_gateway_resource" "api-summoners-resource" {
+  rest_api_id = aws_api_gateway_rest_api.default.id
+  parent_id   = aws_api_gateway_rest_api.default.root_resource_id
+  path_part   = "summoners"
+}
+
+resource "aws_api_gateway_method" "api-summoners-method" {
+    rest_api_id = aws_api_gateway_rest_api.default.id
+    resource_id = aws_api_gateway_resource.api-summoners-resource.id
+    http_method = "ANY"
+    authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "api-summoners-integration" {
+    rest_api_id = aws_api_gateway_rest_api.default.id
+    resource_id = aws_api_gateway_resource.api-summoners-resource.id
+    http_method = aws_api_gateway_method.api-summoners-method.http_method
+    integration_http_method = "POST"
+    type = "AWS_PROXY"
+    uri = data.aws_lambda_function.api-summoners.invoke_arn
+}
+
+resource "aws_lambda_permission" "api-summoners-permission" {
+    statement_id = "AllowAPIGatewayInvoke"
+    action = "lambda:InvokeFunction"
+    function_name = data.aws_lambda_function.api-summoners.function_name
+    principal = "apigateway.amazonaws.com"
+    source_arn = "${aws_api_gateway_rest_api.default.execution_arn}/*/*"
+}

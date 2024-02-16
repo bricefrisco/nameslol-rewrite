07f5815fc8a987c537c2e7e48faf030a16790088
Brice Frisco <39070938+bricefrisco@users.noreply.github.com>
2024-02-15 19:22:56 -0600

    schedule the name updater

--- name-updater/producer/deploy.tf
+++ name-updater/producer/deploy.tf
@@ -53,6 +53,22 @@ resource "aws_iam_role" "lambda_exec" {
   })
 }
 
+resource "aws_iam_role" "scheduler_exec" {
+  name = "${var.app_name}_scheduler_role"
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "scheduler.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role_policy" "lambda_exec_policy" {
   role   = aws_iam_role.lambda_exec.id
   policy = jsonencode({
@@ -76,6 +92,24 @@ resource "aws_iam_role_policy" "lambda_exec_policy" {
         ],
         "Resource" : [
           data.aws_sqs_queue.name-update-queue.arn
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "scheduler_exec_policy" {
+  role = aws_iam_role.scheduler_exec.id
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": [
+          "lambda:InvokeFunction"
+        ],
+        "Effect": "Allow",
+        "Resource": [
+            aws_lambda_function.default.arn
         ]
       }
     ]
@@ -107,3 +141,20 @@ resource "aws_lambda_function" "default" {
   }
 }
 
+resource "aws_scheduler_schedule" "hourly" {
+  name = "name-updater-hourly"
+  schedule_expression = "cron(0 * ? * * *)" // Every hour
+
+  target {
+    arn = aws_lambda_function.default.arn
+    role_arn = aws_iam_role.scheduler_exec.arn
+
+    input = jsonencode({
+      "refreshType": "hourly"
+    })
+
+    retry_policy {
+      maximum_retry_attempts = 0
+    }
+  }
+}

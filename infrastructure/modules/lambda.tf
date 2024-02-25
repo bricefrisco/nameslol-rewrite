028e33da9d407d7110cb0e49356716a68147a6c3
Brice Frisco <39070938+bricefrisco@users.noreply.github.com>
2024-02-24 19:47:12 -0600

    added lambda arn output

--- infrastructure/modules/lambda.tf
+++ infrastructure/modules/lambda.tf
@@ -86,3 +86,8 @@ resource "aws_lambda_function" "default" {
     variables = var.environment_variables
   }
 }
+
+output "lambda_function_arn" {
+  description = "The ARN of the lambda function."
+  value = aws_lambda_function.default.arn
+}
